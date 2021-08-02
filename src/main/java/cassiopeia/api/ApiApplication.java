5fab4777bb43935c76c2e5bff75020ba847d239c
lithelms <hoenzsch.william@gmail.com>
2021-08-02 13:30:35 -0400

    added controller (oops)

--- src/main/java/cassiopeia/api/ApiApplication.java
+++ src/main/java/cassiopeia/api/ApiApplication.java
@@ -6,13 +6,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Controller
 @SpringBootApplication
 public class ApiApplication {
-	@RequestMapping("/ping")
+
+	@RequestMapping("/")
 	@ResponseBody
 	String ping() {
-		return "<h1>Ping-<h1>";
+		return "<h1>Ping<h1>";
 	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
