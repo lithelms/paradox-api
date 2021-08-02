5e81134e20b4ed82d32583136b20aa2ef02b6f00
lithelms <hoenzsch.william@gmail.com>
2021-08-02 13:09:27 -0400

    barebones heroku build

--- src/main/java/cassiopeia/api/ApiApplication.java
+++ src/main/java/cassiopeia/api/ApiApplication.java
@@ -1,11 +1,18 @@
 package cassiopeia.api;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ApiApplication {
-
+	@RequestMapping
+	@ResponseBody
+	String ping() {
+		return "<h1>Works<h1>";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
